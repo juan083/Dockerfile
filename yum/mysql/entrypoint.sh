f68b635acb0036a22c90b5cbf49a0513b9180d40
juan083 <juan083@163.com>
2020-05-02 16:18:13 +0800

    mysql

--- yum/mysql/entrypoint.sh
+++ yum/mysql/entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 DATADIR='/var/lib/mysql'
 LOGDIR='/var/log/mysqld.log'
-if [ ! -d "$DATADIR/mysql" ]; then
-	mkdir -p "$DATADIR"
-	chown -R mysql:mysql "$DATADIR"
+if [[ ! -d "$DATADIR/mysql" ]]; then
+    rm -rf ${DATADIR}
+    mkdir -p "$DATADIR"
+    chown -R mysql:mysql "$DATADIR"
     mysqld --initialize-insecure --user=mysql --datadir=${DATADIR} >> ${LOGDIR}
 fi
-mysqld -d >> ${LOGDIR}
+mysqld --user mysql >> ${LOGDIR}
